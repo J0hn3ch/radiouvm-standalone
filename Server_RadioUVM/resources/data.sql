4aff0a8dcc34ad2658959252e8c64997150d5888
Gianluca Carbone <37618867+J0hn3ch@users.noreply.github.com>
2022-04-12 16:25:16 +0200

    Delete Server_RadioUVM directory

--- Server_RadioUVM/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into user(firstName, lastName, email) values("Giorgio", "Chiellini", "gchiellini@example.it");
-insert into user(firstName, lastName, email) values("Nicolo", "Barella", "nbarella@example.it");
-insert into user(firstName, lastName, email) values("Gianluigi", "Donnarumma", "gdonnarumma@example.it");
-insert into user(firstName, lastName, email) values("Roberto", "Insigne", "rinsigne@example.it");
